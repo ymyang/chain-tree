8382e9ac3904f566c75790f8ea69fcf16ba6dcf2
ymyang <ymyang@163.com>
2016-07-19 17:10:47 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'babel',
                 query: {
-                    presets: ['es2015']
+                    presets: ['es2015','stage-0']
                 },
                 exclude: /node_modules/
             }
